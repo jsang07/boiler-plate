e70543b7d2f3f2e15c47c1115fb17ca5b74366f6
ljs <ljs071420@gmail.com>
2021-03-07 00:33:07 +0900

    pw security

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+// heroku 에서 베포 후 사용?
+module.exports = {
+    mongoURI: process.env.MONGO_URI // 헤로쿠에서 config var 쓸 때 떡같이
+}
--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://js:js1234@boiler-plate.jmdi3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true}));
+// 이 방식들로 데이터 분석? 받게 해준다
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false // 에러 안 뜬다고 함
 }).then(()=> console.log('MGD connected..'))
   .catch(err => console.log(err))
-app.get('/', (req, res) => res.send('Hello World sex'))
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+app.post('/register', (req, res) =>{
+  // 회원 가입 할때 필요한 정보들을 고객에서 가져와서 데이터베이스에 넣는다
+  
+  const user = new User(req.body)    
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, ()=> console.log(`Example port ${port}!`))
